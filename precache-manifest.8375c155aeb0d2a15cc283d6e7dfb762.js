aeeca9a064e88f1ece63357f14d9b59b968b5287
Dmitriy <shirmanovdmitriy@gmail.com>
2019-01-21 21:53:29 +0700

    Updates

---
+++ precache-manifest.8375c155aeb0d2a15cc283d6e7dfb762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "089f26a4706250769144",
+    "url": "/GoT/static/js/runtime~main.089f26a4.js"
+  },
+  {
+    "revision": "ec4645cca2fe37adf720",
+    "url": "/GoT/static/js/main.ec4645cc.chunk.js"
+  },
+  {
+    "revision": "31e556316ebccd8d0588",
+    "url": "/GoT/static/js/1.31e55631.chunk.js"
+  },
+  {
+    "revision": "ec4645cca2fe37adf720",
+    "url": "/GoT/static/css/main.70960412.chunk.css"
+  },
+  {
+    "revision": "31e556316ebccd8d0588",
+    "url": "/GoT/static/css/1.0615b8f4.chunk.css"
+  },
+  {
+    "revision": "e3ff394a876f29ad0be22c11219e3225",
+    "url": "/GoT/index.html"
+  }
+];
