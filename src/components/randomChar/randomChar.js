19666e75f2187568493d02c8c0b466a63893f8f2
Dmitriy <shirmanovdmitriy@gmail.com>
2019-01-23 20:48:07 +0700

    Дубль 121 урок 8

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import styled from 'styled-components';
+import gotService from '../../services'
 
 const RandomBlock = styled.div`
     background-color: #fff;
@@ -16,28 +17,54 @@ const RandomBlock = styled.div`
 `
 
 export default class RandomChar extends Component {
+    constructor(){
+        super()
+        this.updateChar();
+    }
+    gotService  = new gotService();
+    state = {
+        name: null,
+        gender: null,
+        born: null,
+        died: null,
+        culture: null
+    }
 
+    updateChar() {
+        const id = 121;
+        this.gotService.getCharacter(id)
+            .then((char)=>{
+                console.log(char);
+                this.setState({
+                    name: char.name,
+                    gender: char.gender,
+                    born: char.born,
+                    died: char.died,
+                    culture: char.culture
+                })
+            })
+    }
     render() {
-
+        const {name, gender, born, died, culture} =this.state;
         return (
             <RandomBlock className="rounded">
-                <h4>Random Character: John</h4>
+                <h4>{name}</h4>
                 <ListGroup className="list-group-flush">
                     <ListGroupItem className="d-flex justify-content-between">
                         <span className="term">Gender </span>
-                        <span>male</span>
+                        <span>{gender}</span>
                     </ListGroupItem>
                     <ListGroupItem className="d-flex justify-content-between">
                         <span className="term">Born </span>
-                        <span>11.03.1039</span>
+                        <span>{born}</span>
                     </ListGroupItem>
                     <ListGroupItem className="d-flex justify-content-between">
                         <span className="term">Died </span>
-                        <span>13.09.1089</span>
+                        <span>{died}</span>
                     </ListGroupItem>
                     <ListGroupItem className="d-flex justify-content-between">
                         <span className="term">Culture </span>
-                        <span>Anarchy</span>
+                        <span>{culture}</span>
                     </ListGroupItem>
                 </ListGroup>
             </RandomBlock>
